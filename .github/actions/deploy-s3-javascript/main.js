0b3af444799fd32496b8fe296fc3c0f4c3c9073b
Taimoor Haider <tamiraja823@gmail.com>
2025-04-07 17:31:16 +0500

    Add custom js deploymnet action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,14 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  const awsUri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${awsUri} --region ${bucketRegion}`);
   core.notice("Loggin onto the github action console.");
 }
 run();
